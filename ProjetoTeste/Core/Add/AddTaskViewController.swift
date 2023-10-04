c0bded34cbb24e41d7985554d380f4bac1a933a6
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-04 14:07:35 -0300

    feature(Storyboard): Connecting storyboard add components with AddViewController

---
+++ ProjetoTeste/Core/Add/AddTaskViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddTaskViewController.swift
+//  ProjetoTeste
+//
+//  Created by thaxz on 04/10/23.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+    
+    // MARK: Components
+    @IBOutlet var addTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func saveNewTask(_ sender: UIButton) {
+    }
+    
+   
+
+}
