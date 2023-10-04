28e196330734ed26cee33a5941474768dbc4285b
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-04 14:22:41 -0300

    feature(Storyboard): fix: adding description

--- ProjetoTeste/Core/Details/TaskDetailViewController.swift
+++ ProjetoTeste/Core/Details/TaskDetailViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class TaskDetailViewController: UIViewController {
+    
+    // MARK: Components
 
+    @IBOutlet var taskDescription: UILabel!
+    @IBOutlet var taskCreationDate: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +21,6 @@ class TaskDetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
